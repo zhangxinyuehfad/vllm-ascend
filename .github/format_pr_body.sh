8a6d1c7a77afa4538d96340c554f23eb5a7e4924
Yikun Jiang <yikunkero@gmail.com>
2025-07-04 12:05:14 +0800

    Record commit test
    Signed-off-by: Yikun Jiang <yikunkero@gmail.com>

---
+++ .github/format_pr_body.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eux
+
+# ensure 2 argument is passed
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <pr_number> <new_message>"
+    exit 1
+fi
+
+PR_NUMBER=$1
+NEW_MESSAGE=$2
+OLD=/tmp/orig_pr_body.txt
+NEW=/tmp/new_pr_body.txt
+
+gh pr view --json body --template "{{.body}}" "${PR_NUMBER}" > "${OLD}"
+cp "${OLD}" "${NEW}"
+
+# Remove "FIX #xxxx (*link existing issues this PR will resolve*)"
+sed -i '/<!--/,/-->/d' "${NEW}"
+echo "$NEW_MESSAGE" >> "${NEW}"
+
+# Run this only if ${NEW} is different than ${OLD}
+if ! cmp -s "${OLD}" "${NEW}"; then
+    echo
+    echo "Updating PR body:"
+    echo
+    cat "${NEW}"
+    # Enable this after workflow merged
+    curl -X PATCH \
+        -H "Authorization: token ${PR_TOKEN}" \
+        -H "Accept: application/vnd.github.v3+json" \
+        -d '{"body":"NEW_DESCRIPTION_HERE"}' \
+        https://api.github.com/repos/Yikun/vllm-ascend/pulls/"${PR_NUMBER}"
+    # gh pr edit --body-file "${NEW}" "${PR_NUMBER}"
+else
+    echo "No changes needed"
+fi
